592b517331024705abe4aa1f95a8aadc625cc66e
Dietrich <dietrich.poensgen@heg-portal.de>
2021-02-07 13:57:53 +0100

    new build

---
+++ .routify/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": true,
+  "singleBuild": true,
+  "noHashScroll": false,
+  "distDir": "dist",
+  "hashScroll": true,
+  "extensions": [
+    "svelte",
+    "html",
+    "svx",
+    "md"
+  ],
+  "started": "2021-02-07T12:55:33.602Z"
+}
